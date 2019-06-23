db33512a0c63ac10a9273ea9e92f247871e96406
SHEFFmachine <sheff@sheffmachine.com>
2019-06-22 19:01:43 -0700

    do soem more work around looking for items on a web page.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,17 +3,35 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.List;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "/opt/local/bin/chromedriver");
         WebDriver browser = new ChromeDriver();
         browser.get("https://www.saucelabs.com");
         WebElement href = browser.findElement(By.xpath("//a[@href='/beta/login']"));
+        List<WebElement> body = browser.findElements(By.tagName("div"));
         System.out.println("==========================================");
         System.out.println("Page source is below:");
+        IntStream streamOfPage = browser.getPageSource().chars();
         System.out.println(browser.getPageSource());
         System.out.println("==========================================");
         System.out.println("I visited website and got the href " + href.isDisplayed());
+
+
+        System.out.println("==========================================");
+        boolean containsA =
+                Pattern.compile(", ").splitAsStream("a, b, c").parallel().distinct().anyMatch(c -> c.equals("a"));
+
+//        String outcome = streamOfString.collect(Collectors.joining(": "));
+
+
+        System.out.println("Stream of string contains Continuous " + body.parallelStream().anyMatch(s -> s.getText().contains("Continuous")));
 
         browser.close();
     }
